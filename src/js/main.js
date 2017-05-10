df00c0307ea7d2a6042f6442172f860d865254d3
Stravskyy <stravskyy@gmail.com>
2017-05-10 13:54:56 +0300

    Finished js work

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+var nameInput = prompt("Enter your name:");
+var nameOutput = document.getElementById("name");
+nameOutput.innerText = nameInput;
+
+var ageInput = prompt("Enter your age:");
+var ageOutput = document.getElementById("age");
+ageOutput.innerText = ageInput;
+
+var adressInput = prompt("Enter your adress:");
+var adressOutput = document.getElementById("adress");
+adressOutput.innerText = adressInput;
+
+var phoneNumberInput = prompt("Enter your phone number:");
+var phoneNumberOutput = document.getElementById("phoneNumber");
+phoneNumberOutput.innerText = phoneNumberInput;
